661a3f854f393e983e6fcf626a0dd760c87846f6
Jiawei Zhao <97944674+zhaojiawei1223@users.noreply.github.com>
2022-05-17 16:55:41 +0200

    Add files via upload

---
+++ Jiawei's-approach/Data augmentation.py
@@ -0,0 +1,50 @@
+# Data augmentation was performed to expand image dataset and to reduce overfitting.
+
+# set parameters of data augmentation
+seed=24
+from keras.preprocessing.image import ImageDataGenerator
+
+img_data_gen_args = dict(rotation_range=20,
+                         width_shift_range=0.1,
+                         height_shift_range=0.1,
+                         shear_range=0.1,
+                         horizontal_flip=True,
+                         vertical_flip=True,
+                         fill_mode='reflect',
+                         preprocessing_function = preprocess_input)
+
+mask_data_gen_args = dict(rotation_range=20,
+                          width_shift_range=0.1,
+                          height_shift_range=0.1,
+                          shear_range=0.1,
+                          horizontal_flip=True,
+                          vertical_flip=True,
+                          fill_mode='reflect',
+                          preprocessing_function = lambda x: np.where(x > 0, 1, 0).astype(x.dtype))
+
+# set the batch size of image generator
+batch_size = 8
+
+# image generator (on X_train and X_test)
+image_data_generator = ImageDataGenerator(**img_data_gen_args)
+image_data_generator.fit(X_train, augment=True, seed=seed)  # load X_train before this
+
+image_generator = image_data_generator.flow(X_train, seed=seed, batch_size=batch_size)
+valid_img_generator = image_data_generator.flow(X_test, seed=seed, batch_size=batch_size)
+
+# mask generator (on y_train and y_test)
+mask_data_generator = ImageDataGenerator(**mask_data_gen_args)
+mask_data_generator.fit(y_train, augment=True, seed=seed)  # load y_train before this
+
+mask_generator = mask_data_generator.flow(y_train, seed=seed, batch_size=batch_size)
+valid_mask_generator = mask_data_generator.flow(y_test, seed=seed, batch_size=batch_size)
+
+# put image generator and mask generator together
+def my_image_mask_generator(image_generator, mask_generator):
+    train_generator = zip(image_generator, mask_generator)
+    for (img, mask) in train_generator:
+        yield (img, mask)
+
+train_generator = my_image_mask_generator(image_generator, mask_generator)
+validation_generator = my_image_mask_generator(valid_img_generator, valid_mask_generator)
+# train_generator and validation_generator can be directly used in model.fit()
